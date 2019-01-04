6d3d44624121fcb596d38eda54a6502105bd84e8
Brian Ruff <thelonewolf198927@gmail.com>
2019-01-03 20:38:49 -0500

    make basic user model and include password digest with has_secure_password

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  before_save { self.email.downcase! } # works for update and # create
+
+  validates(:name, presence: true, length: { minimum: 3, maximum: 50 })
+
+  # REGEX stored in config/initializers/constants.rb file
+  validates(:email, presence: true, length: { minimum: 3, maximum: 50 },
+            format: { with: VALID_EMAIL_REGEX }, uniqueness: {
+          case_sensitive: false })
+
+  validates(:password, presence: true, length: { minimum: 6 })
+
+  has_secure_password
+end
