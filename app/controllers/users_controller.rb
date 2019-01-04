037ccb5afa02bda99ba7ee77e610b0e76d3c6f5e
Brian Ruff <thelonewolf198927@gmail.com>
2019-01-04 03:21:44 -0500

    update for error handling and styling for error handling and using the .errors object on invalid request as well as implementing strong params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,19 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      ### ...Handle a successful save... ###
+    else
+      render new_user_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
